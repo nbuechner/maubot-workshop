58bf32e5f60b64f1fc844d3ab56ffa0692b7cd4c
Nils Büchner <n.buechner@ubuntu.com>
2024-10-19 17:33:34 +0200

    add subcommand

--- workshopbot/workshopbot.py
+++ workshopbot/workshopbot.py
@@ -3,6 +3,13 @@ from maubot.handlers import command
 
 
 class WorkshopBot(Plugin):
-  @command.new(name="hello") 
+  @command.new(name="welcome") 
   async def hello_world(self, evt: MessageEvent) -> None:
-    await evt.reply("Hello, World!")
+    await evt.reply("Welcome to the Matrix Workshop!")
+
+  @command.new(name="hello")
+  @command.argument(name="name", pass_raw=True, required=False)  # Optional argument
+  async def hello_world(self, evt: MessageEvent, name: str = "World") -> None:
+      # If no name is provided, use the default "World"
+      greeting = f"Hello, {name or 'World'}!"
+      await evt.reply(greeting)
